1bed3769d24290bb6db3547d2c23e8d45a332b63
JunhwanKang <wnsghks1017@naver.com>
2020-10-03 17:06:53 +0900

    xml을 이용한 DI

--- diexam01/src/main/java/kr/or/connect/diexam01/ApplicationContextExam01.java
+++ diexam01/src/main/java/kr/or/connect/diexam01/ApplicationContextExam01.java
@@ -16,7 +16,7 @@ public class ApplicationContextExam01 {
 		UserBean userBean2 = (UserBean)ac.getBean("userBean");
 		
 		if(userBean == userBean2)
-			System.out.println("같은 인스턴스입니다.");
+			System.out.println("같은 인스턴스입니다."); //싱클턴 패턴이므로 객체를 계속 만들어 내는게 아니라 하나 만든 bean을 이용한다.
 	}
 
 }
